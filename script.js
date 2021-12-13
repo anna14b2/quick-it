d3328ba79dfec0c7381fe8aec944603df97128e0
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-12-13 13:46:44 +0100

    showing differnet steps for management app

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// burger menu
 const btn = document.getElementById("menu-btn");
 const nav = document.getElementById("menu");
 
@@ -8,3 +9,99 @@ function navToggle() {
 }
 
 btn.addEventListener("click", navToggle);
+
+// management app - step 1,2,3,4
+const step1 = document.getElementById("step1");
+const step2 = document.getElementById("step2");
+const step3 = document.getElementById("step3");
+const step4 = document.getElementById("step4");
+
+const stepA = document.getElementById("stepA");
+const stepB = document.getElementById("stepB");
+const stepC = document.getElementById("stepC");
+const stepD = document.getElementById("stepD");
+
+// function getStepA() {
+//   stepA.classList.remove("hideme");
+//   stepA.classList.add("z-index");
+//   stepB.classList.add("hideme");
+//   step2.classList.remove("selected");
+//   step1.classList.add("selected");
+// }
+
+// step1.addEventListener("click", getStepA);
+
+// function getStepB() {
+//   stepB.classList.remove("hideme");
+//   stepB.classList.add("z-index");
+//   stepA.classList.add("hideme");
+//   step1.classList.remove("selected");
+//   step2.classList.add("selected");
+// }
+
+// step2.addEventListener("click", getStepB);
+
+// function getStepC() {
+//   stepC.classList.remove("hideme");
+//   stepC.classList.add("z-index");
+//   stepA.classList.add("hideme");
+//   stepB.classList.add("hideme");
+//   step1.classList.remove("selected");
+//   step2.classList.remove("selected");
+//   step3.classList.add("selected");
+// }
+
+// step3.addEventListener("click", getStepC);
+
+function getStepA() {
+  stepA.classList.remove("hideme");
+  stepA.classList.add("z-index");
+  stepD.classList.add("hideme");
+  stepB.classList.add("hideme");
+  stepC.classList.add("hideme");
+  step4.classList.remove("selected");
+  step2.classList.remove("selected");
+  step3.classList.remove("selected");
+  step1.classList.add("selected");
+}
+
+function getStepB() {
+  stepB.classList.remove("hideme");
+  stepB.classList.add("z-index");
+  stepA.classList.add("hideme");
+  stepD.classList.add("hideme");
+  stepC.classList.add("hideme");
+  step1.classList.remove("selected");
+  step4.classList.remove("selected");
+  step3.classList.remove("selected");
+  step2.classList.add("selected");
+}
+
+function getStepC() {
+  stepC.classList.remove("hideme");
+  stepC.classList.add("z-index");
+  stepA.classList.add("hideme");
+  stepB.classList.add("hideme");
+  stepD.classList.add("hideme");
+  step1.classList.remove("selected");
+  step2.classList.remove("selected");
+  step4.classList.remove("selected");
+  step3.classList.add("selected");
+}
+
+function getStepD() {
+  stepD.classList.remove("hideme");
+  stepD.classList.add("z-index");
+  stepA.classList.add("hideme");
+  stepB.classList.add("hideme");
+  stepC.classList.add("hideme");
+  step1.classList.remove("selected");
+  step2.classList.remove("selected");
+  step3.classList.remove("selected");
+  step4.classList.add("selected");
+}
+
+step1.addEventListener("click", getStepA);
+step2.addEventListener("click", getStepB);
+step3.addEventListener("click", getStepC);
+step4.addEventListener("click", getStepD);
